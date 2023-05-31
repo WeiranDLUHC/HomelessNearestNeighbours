35f357e0c642e00eefaba3ef9045a5cd5672301c
WeiranDLUHC <94060471+WeiranDLUHC@users.noreply.github.com>
2023-05-31 21:32:50 +0100

    Nearest neighbour code

---
+++ Code/Load input data.R
@@ -0,0 +1,33 @@
+## Clear workspace and set directory
+
+setwd(
+  paste0(
+    "Q:/ASCT/P005 Homelessness/007 Stat homeless/Data and stats development project/",
+    "002 Deliverables/024 Automation/HomelessNearestNeighbours"
+  )
+)
+
+# load in packages
+
+library(readxl)
+library(tidyverse)
+library(ggplot2)
+library(scales)
+
+# load input data
+
+NN_inputs <-
+  read_excel("Files/Input Data.xlsx", sheet = "NN Raw Data")
+
+
+LA_names <-
+  read_excel("Files/Input Data.xlsx", sheet = "Area Names")
+NN_inputs<-LA_names%>%
+  left_join(NN_inputs,by=c("LA Code"="Area Code"))%>%
+  select(-c(`Area Name`,`LA Code`))
+
+
+NN_inputs<-as.data.frame(NN_inputs)
+rownames(NN_inputs)<-NN_inputs[,1]
+NN_inputs[,1] <- NULL
+NN_inputs[]<-as.data.frame(lapply(NN_inputs,as.numeric))
---
+++ Code/Nearest neighbours calculation.R
@@ -0,0 +1,38 @@
+## Calculates nearest neighbours
+# Dependencies: Load input data
+
+nvars<-ncol(NN_inputs) 
+nlas<-nrow(NN_inputs) 
+scaled_NN_inputs<-scale(NN_inputs) #scale the CIPFA data matrix (standardize)
+
+#create a min max normalization function
+min_max_norm <- function(x) {
+  (x - min(x)) / (max(x) - min(x))
+}
+
+#create a empty final matrix to input the data
+final_matrix<-matrix(0L,nrow=nlas,ncol=nlas) #nrows and ncol is the number of areas
+
+# create a distance matrix for each column of the CIPFA feature
+# standardize each of the columns so that 0 is the nearest neighbour and 1  is the furthest
+# add all the standardized matrix for each column together 
+
+for (i in 1:nvars){
+  dis_matrix<-dist(scaled_NN_inputs[,i])
+  dis_matrix<-as.data.frame(as.matrix(dis_matrix))
+  dis_matrix[]<- as.data.frame(lapply(dis_matrix, min_max_norm))
+  
+  final_matrix<-final_matrix+dis_matrix
+}#note that 1:X, where x is the number of variables
+
+#Divide by the number of columns and get the topN rownames for each column
+# save the output
+# 20 used as 20 nearest neighbours
+
+f<-function(x) names(sort(x,decreasing=F))
+final_matrix<-final_matrix/20
+final_matrix_names<-apply(final_matrix,2,f)
+
+# clean up final matrix
+final_matrix_names <- rbind(colnames(final_matrix_names), final_matrix_names)
+colnames(final_matrix_names) <- paste0('V', seq_len(ncol(final_matrix_names)))
